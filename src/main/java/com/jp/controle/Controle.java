aadedec7ee3fcaf13e1c601b9851dcdc1810077a
Andre-Luiz01 <andreff.net@gmail.com>
2023-09-11 21:22:58 -0300

    Final dia 1

--- src/main/java/com/jp/controle/Controle.java
+++ src/main/java/com/jp/controle/Controle.java
@@ -4,33 +4,37 @@ import com.jp.modelos.Contador;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Controle implements IControle{
     @Override
     public Contador arvorizar(String caminhoDoTXT) throws Exception {
         String texto = limpaTexto(caminhoDoTXT);
         texto = retiraStop(texto);
+        String[] textoSeparado = texto.split(" ");
 
         return null;
     }
 
     private String limpaTexto(String caminhoDoTXT) throws FileNotFoundException {
         String textoBruto = new Scanner(new File(caminhoDoTXT), "UTF-8").useDelimiter("\\A").next();
+        textoBruto = textoBruto.replaceAll("[^\\w\\s]", "");
         while(textoBruto.contains("  ")) {
             textoBruto = textoBruto.replace("  ", " ");
             textoBruto = textoBruto.replace(System.lineSeparator(), " ");
         }
-        String textoLimpo = textoBruto = textoBruto.trim();
+        String textoLimpo = textoBruto.trim();
         return textoLimpo;
     }
 
     private String retiraStop(String textoBruto) throws Exception{
-        String[] StopWords = new Scanner(new File("../Stopwords/stopwords.txt"), "UTF-8").useDelimiter("\\A").next().split(System.lineSeparator());
+        textoBruto = " " + textoBruto + " ";
+        String[] StopWords = new Scanner(new File(getClass().getResource("../arquivos/stopwords.txt").getFile()), "UTF-8").useDelimiter("\\A").next().split(" \n");
         for (String palavra :
                 StopWords) {
-            textoBruto.replace(palavra, "");
+            textoBruto = textoBruto.replace(" "+ palavra + " ", " ");
         }
-        System.out.println(textoBruto);
+        textoBruto = textoBruto.trim();
         return textoBruto;
     }
 }
