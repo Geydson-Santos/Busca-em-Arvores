fe988bf993348af967a9aef062594503d2c345a9
Geydson <geydson87@gmail.com>
2023-09-12 23:47:24 -0300

    mudanças na tela

--- src/main/java/com/jp/visao/TelaOcorrências.java
+++ src/main/java/com/jp/visao/TelaOcorrências.java
@@ -4,13 +4,18 @@ import com.jp.modelos.Palavra;
 import com.sun.source.tree.TryTree;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.FileChooser;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.net.URL;
 import java.util.*;
 //import java.util.
@@ -26,6 +31,29 @@ public class TelaOcorrências implements Initializable {
 
     @FXML
     private TableColumn<?, ?> colunaPalavra;
+
+    @FXML
+    void extrairRelatorio(ActionEvent event) {
+        FileChooser fc = new FileChooser();
+        fc.setTitle("Abrir arquivo de texto");
+        fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("Arquivo de Texto", "*.txt"));
+        File arquivo = fc.showSaveDialog(this.TabelaOcorrencias.getScene().getWindow());
+
+        if(arquivo != null){
+            try{
+                FileWriter fw = new FileWriter(arquivo);
+                BufferedWriter bw = new BufferedWriter(fw);
+
+                for (Palavra palavra: TabelaOcorrencias.getItems()) {
+                    bw.write(palavra.getPalavra() + "\t" + palavra.getOcorrencias() + "\n");
+                }
+
+                bw.close();
+            }catch (Exception erro) {
+                erro.printStackTrace();
+            }
+        }
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
